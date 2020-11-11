9154d4114bbbe3d1dc682accda2e2ac9ea59d0c9
vagrant <vagrant@vet>
2020-11-11 11:08:02 +0000

    Edits to updated telephone numbers migration

--- database/migrations/2020_11_10_211706_update_telephone_length_on_owners_table.php
+++ database/migrations/2020_11_10_211706_update_telephone_length_on_owners_table.php
@@ -26,7 +26,7 @@ class UpdateTelephoneLengthOnOwnersTable extends Migration
     public function down()
     {
         Schema::table('owners', function (Blueprint $table) {
-            $table->string("telephone", 11)->change;
+            $table->string("telephone", 11)->change();
         });
     }
 }
